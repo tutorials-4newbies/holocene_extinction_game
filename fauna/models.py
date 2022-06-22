520d75494be18554529e69d52a769cbb417c63ac
alonisser <alonisser@gmail.com>
2022-06-23 00:06:07 +0300

    choices

--- fauna/models.py
+++ fauna/models.py
@@ -4,10 +4,19 @@ from django.db import models
 # Create your models here.
 
 class Animal(models.Model):
+    PERIOD_CHOICES = [
+        ("PERMIAN", 'Permian'),
+        ("TRIASSIC", 'Triassic'),
+        ("JURASSIC", 'Jurassic'),
+        ("CRETACEOUS", 'Cretaceous'),
+        ("PALEOGENE", 'Paleogene'),
+        ("NEOGENE", 'Neogene'),
+        ("QUATERNARY", 'Quaternary'),
+    ]
 
     name = models.CharField(max_length=50, blank=False, null=False)
     extinction = models.CharField(max_length=50, blank=False, null=False)
-    period = models.CharField(max_length=50, blank=False, null=False)
+    period = models.CharField(max_length=50, blank=False, null=False, choices=PERIOD_CHOICES)
     taxonomy_class = models.CharField(max_length=50, blank=False, null=False)
     taxonomy_order = models.CharField(max_length=50, blank=False, null=False)
     taxonomy_family = models.CharField(max_length=50, blank=False, null=False)
