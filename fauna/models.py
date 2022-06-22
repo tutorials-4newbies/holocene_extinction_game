a5b6e24ec267a6a9aaff52181dcc8a19eb721f79
alonisser <alonisser@gmail.com>
2022-06-22 10:38:59 +0300

    change model

--- fauna/models.py
+++ fauna/models.py
@@ -8,7 +8,10 @@ class Animal(models.Model):
     name = models.CharField(max_length=50, blank=False, null=False)
     extinction = models.CharField(max_length=50, blank=False, null=False)
     period = models.CharField(max_length=50, blank=False, null=False)
-    animal_class = models.CharField(max_length=50, blank=False, null=False)
-    order = models.CharField(max_length=50, blank=False, null=False)
-    family = models.CharField(max_length=50, blank=False, null=False)
+    taxonomy_class = models.CharField(max_length=50, blank=False, null=False)
+    taxonomy_order = models.CharField(max_length=50, blank=False, null=False)
+    taxonomy_family = models.CharField(max_length=50, blank=False, null=False)
+
+    def __str__(self):
+        return f"{self.name} of {self.taxonomy_family}"
 
