efdc47681fb38021f647aa6de9e1909d5df10cbf
alonisser <alonisser@gmail.com>
2022-06-22 12:02:50 +0300

    a bit more advanced admin

--- fauna/admin.py
+++ fauna/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from fauna.models import Animal
+
+
+class AnimalAdmin(admin.ModelAdmin):
+    list_display = ['name', 'extinction', 'period', 'taxonomy_class', 'taxonomy_class', 'taxonomy_family']
+    list_filter = ['period']
+    search_fields = ['name']
+    list_editable = ['extinction']
+    
+
+admin.site.register(Animal, AnimalAdmin)
