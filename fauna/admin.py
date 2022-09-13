0e19cce99852ad0b101b70983d84e526c429f89c
alonisser <alonisser@gmail.com>
2022-09-14 00:10:00 +0300

    more admin

--- fauna/admin.py
+++ fauna/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db.models import QuerySet
 from import_export.admin import ImportExportMixin
 # Register your models here.
 from fauna.models import Animal
@@ -11,12 +12,20 @@ class AnimalAdmin(ImportExportMixin, admin.ModelAdmin):
     list_filter = ['period']
     search_fields = ['name']
     list_editable = ['extinction']
+    actions = ["upgrade_period"]
 
     def get_queryset(self, request):
         qs = super().get_queryset(request=request)
         if settings.PERIOD != 'all':
             qs = qs.filter(period=settings.PERIOD)
         return qs
-    
+
+    admin.action(description='Upgrade the period')
+    def upgrade_period(self, request, queryset):
+        for obj in queryset:
+            if obj.period == Animal.PERIOD_CHOICES[0]:
+                obj.period = Animal.PERIOD_CHOICES[1]
+                obj.save()
+
 
 admin.site.register(Animal, AnimalAdmin)
