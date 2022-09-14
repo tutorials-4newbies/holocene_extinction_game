9d288f320b44a102a071f0482850734cdfc179e8
alonisser <alonisser@gmail.com>
2022-09-14 09:08:22 +0300

    Starting with rest framework

--- fauna/tests.py
+++ fauna/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
 # Create your tests here.
+class AnimalTestCase(APITestCase):
+    def setUp(self) -> None:
+        pass
+
+    def tearDown(self) -> None:
+        pass
+
+    def test_get_empty_list_of_animals_on_endpoint(self):
+        res = self.client.get(reverse("animals-list"))
+        self.assertEqual(status.HTTP_200_OK, res.status_code)
+        self.assertEqual(len(res.data), 0)
--- fauna/views.py
+++ fauna/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.permissions import AllowAny
+from rest_framework.viewsets import ReadOnlyModelViewSet
+
+from fauna.models import Animal
+from fauna.serializers import AnimalSerializer
+
+
+class AnimalsView(ReadOnlyModelViewSet):
+    queryset = Animal.objects.all()
+
+    serializer_class = AnimalSerializer
+    permission_classes = (AllowAny,)
