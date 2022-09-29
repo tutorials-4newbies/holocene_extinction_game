edec5dc17bdb2ac531f6335e68e5d110ab77f952
omer bental <ombental@gmail.com>
2022-09-29 21:48:07 +0300

    write generic test

--- fauna/tests.py
+++ fauna/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class AnimalViewsetTestCase(APITestCase):
+    
+    def setUp(self) -> None:
+        super(AnimalViewsetTestCase, self).setUp()
+    
+    def tearDown(self) -> None:
+        super(AnimalViewsetTestCase, self).tearDown()
+
+    def test_animal_viewset_api_endpoint(self):
+        res = self.client.get(reverse('animals-list'))
+        self.assertEqual(res.status_code, 200)
+        self.assertIn('results', res.json())
