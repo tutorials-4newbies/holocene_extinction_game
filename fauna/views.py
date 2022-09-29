e2aada9e931e60556d330ca1f5ad907878f9d975
omer bental <ombental@gmail.com>
2022-09-29 21:45:13 +0300

    create animal viewset

--- fauna/views.py
+++ fauna/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from fauna.models import Animal
+from fauna.serializers import AnimalSerializer
+
+
+class AnimalViewSet(viewsets.ReadOnlyModelViewSet):
+
+    serializer_class = AnimalSerializer
+    permission_classes = [AllowAny]
+    queryset = Animal.objects.all()
