94b61fbad5aece00cdb1a461da79e22f89b8a6ea
elam91 <elam91@gmail.com>
2022-10-06 12:38:45 +0300

    customized get serializer class

--- fauna/views.py
+++ fauna/views.py
@@ -1,12 +1,24 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from django.shortcuts import render
 from rest_framework.permissions import AllowAny
+from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ReadOnlyModelViewSet
 # Create your views here.
 from fauna.models import Animal
-from fauna.serializers import AnimalSerializer
+from fauna.serializers import AnimalSerializer, AnonymousAnimalSerializer
 
 
 class AnimalViewSet(ReadOnlyModelViewSet):
     queryset = Animal.objects.all()
     permission_classes = [AllowAny]
-    serializer_class = AnimalSerializer
+    serializer_class = AnonymousAnimalSerializer
+
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ["period"]
+    search_fields = ["name"]
+
+    def get_serializer_class(self):
+        serializer_class = super().get_serializer_class()
+        if self.request.user.is_authenticated:
+            serializer_class = AnimalSerializer
+        return serializer_class
