4bfc44e01662e20476f9727b6a8d1b5dcae08ce3
alonisser <alonisser@gmail.com>
2023-01-18 00:58:48 +0200

    creating profile with signals

--- fauna/apps.py
+++ fauna/apps.py
@@ -1,6 +1,16 @@
 from django.apps import AppConfig
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+
+
+def setup_profile(sender, instance, created, *args, **kwargs):
+    from fauna.models import Profile
+    Profile.create_profile(user=instance)
 
 
 class FaunaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'fauna'
+
+    def ready(self):
+        post_save.connect(receiver=setup_profile, sender=get_user_model())
--- fauna/models/profile.py
+++ fauna/models/profile.py
@@ -7,3 +7,8 @@ class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=CASCADE, related_name="profile")
     is_corp_worker = models.BooleanField(default=False)
     is_grandmaster = models.BooleanField(default=False)
+
+    @classmethod
+    def create_profile(cls, user: object) -> object:
+        profile = Profile.objects.create(user=user)
+        return profile
