28bdc1f9303bc98b06029b83b8e50d7b84af29af
alonisser <alonisser@gmail.com>
2023-02-02 00:01:48 +0200

    positional args

--- fauna/management/commands/resurrect.py
+++ fauna/management/commands/resurrect.py
@@ -1,8 +1,16 @@
 from django.core.management.base import BaseCommand
 
+from fauna.models import Animal
+
 
 class Command(BaseCommand):
     help = "Kathulu sleeps under the see"
 
-    def handle(self, *args, **options):
-        self.stdout.write("He has awaken")
+    def add_arguments(self, parser):
+        #  POSITIONAL argument
+        parser.add_argument("wish", help="what he would wish to do", default="to sleep under the see", type=str)
+
+    def handle(self, *args, **kwargs):
+        extinct_animals = Animal.objects.all()
+        for animal in extinct_animals:
+            self.stdout.write(f"He has awaken {animal.name} in order to {kwargs.get('wish')}")
