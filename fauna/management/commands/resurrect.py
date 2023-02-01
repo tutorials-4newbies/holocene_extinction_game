bc8ac5c62c4331f2a63ca677c44430a731fb18f1
alonisser <alonisser@gmail.com>
2023-02-02 00:02:51 +0200

    positional args

--- fauna/management/commands/resurrect.py
+++ fauna/management/commands/resurrect.py
@@ -9,6 +9,7 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         #  POSITIONAL argument
         parser.add_argument("wish", help="what he would wish to do", default="to sleep under the see", type=str)
+        parser.add_argument("-c", "--color", help="Add some color to life")
 
     def handle(self, *args, **kwargs):
         extinct_animals = Animal.objects.all()
